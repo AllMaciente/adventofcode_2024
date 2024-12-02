914b802ccea1b83d9cdf41c4f25e8d152e9eac9b
All <allmacdev@gmail.com>
2024-12-02 18:13:28 -0300

    day2#part2

--- day2.py
+++ day2.py
@@ -1,13 +1,13 @@
 from utils import get_puzzle_input
 
-puzzleInput = get_puzzle_input.get_input("https://adventofcode.com/2024/day/2/input")
-# puzzleInput = """7 6 4 2 1
-# 1 2 7 8 9
-# 9 7 6 2 1
-# 1 3 2 4 5
-# 8 6 4 4 1
-# 1 3 6 7 9
-# """
+puzzleInput = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"""
+# puzzleInput = get_puzzle_input.get_input("https://adventofcode.com/2024/day/2/input")
 lista = []
 reportsSafe = 0
 a = puzzleInput.strip().split("\n")
@@ -16,24 +16,33 @@ for i in a:
 # print(a)
 # print(lista)
 
-for i in lista:
+
+def isSafe(i):
     check = 0
     if i[0] < i[1]:
         for j in range(1, len(i)):
             if (int(i[j]) - int(i[j - 1])) <= 3 and (int(i[j]) - int(i[j - 1])) >= 1:
                 check += 1
             else:
-                break
+                return False
 
     elif i[0] > i[1]:
         for j in range(1, len(i)):
             if (int(i[j - 1]) - int(i[j])) <= 3 and (int(i[j - 1]) - int(i[j])) >= 1:
                 check += 1
             else:
-                break
+                return False
     else:
-        continue
-    if check == len(i) - 1:
-        reportsSafe += 1
+        return False
+    
+    return check >=len(i) -1
 
+
+for i in lista:
+    if isSafe(i):    
+        reportsSafe += 1
+    
+    for j in  range(1,len(i) - 1):
+        modifiedReport = i[:j] + [j+1:]
+    
 print(reportsSafe)
